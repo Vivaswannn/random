ee2bd5706398eafe2da5f5358dd3125989f095c9
Vivaswannn <vivaswan2002@gmail.com>
2024-10-21 12:26:11 +0530

    without recursion

---
+++ file.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+# Test the function
+number = 5
+print(f"Factorial of {number} is {factorial(number)}")
